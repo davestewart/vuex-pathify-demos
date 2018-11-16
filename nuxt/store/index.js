e784fb8b07175feab8ca8d9183d9846500f77b4a
Özüm Eldoğan <ozum@ozum.net>
2018-11-16 15:35:14 +0300

    Updated to nuxt 2

--- nuxt/store/index.js
+++ nuxt/store/index.js
@@ -1,31 +1,12 @@
-import Vuex from 'vuex'
 import pathify from 'vuex-pathify'
-
-import hello from './modules/hello-world'
-import counter from './modules/counter'
 
 console.log('STORE FILE LOADED!')
 
-const createStore = () => {
+export const plugins = [ pathify.plugin ];
+export const state = () => ({});
 
-  console.log('CREATE STORE!')
-
-  return new Vuex.Store({
-
-    plugins: [ pathify.plugin ],
-
-    modules: {
-      hello,
-      counter,
-    },
-
-    actions: {
-      nuxtServerInit(context, value) {
-        console.log('NUXT_SERVER_INIT!')
-      }
-    }
-
-  })
+export const actions = {
+  nuxtServerInit() {
+    console.log('NUXT_SERVER_INIT!')
+  }
 }
-
-export default createStore
